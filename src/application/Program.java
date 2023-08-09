28bd5cdd3f4ba12be5aa41849527c6c0451a1cc5
Hemmerson Rosa <hemmerson.rosa@gmail.com>
2023-08-09 17:00:47 -0300

    Primeira solução bem ruim

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe os dados da conta: ");
+		System.out.print("Número: ");
+		int number = sc.nextInt();
+		System.out.print("Titular: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Saldo Inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Informe uma quantia para sacar: ");
+		double amount = sc.nextDouble();
+		
+		if(amount > acc.getWithdrawLimit()) {
+			System.out.println("Erro de saque: A quantia excede o limite de saque");
+		}
+		else if(amount > acc.getBalance()) {
+			System.out.println("Erro de saque: Saldo insuficiente");
+		}
+		else {
+			acc.withdraw(amount);
+			System.out.println("Novo saldo: "+ String.format("%.2f", acc.getBalance()));
+		}
+		sc.close();
+	}
+
+}
