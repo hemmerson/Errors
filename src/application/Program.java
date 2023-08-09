817d454c4cfc26040746d8a0768694fd85354538
Hemmerson Rosa <hemmerson.rosa@gmail.com>
2023-08-09 17:34:48 -0300

    Terceira solução, Boa

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Account;
+import exceptions.BusinessException;
 
 public class Program {
 
@@ -28,13 +29,12 @@ public class Program {
 		System.out.print("Informe uma quantia para sacar: ");
 		double amount = sc.nextDouble();
 		
-		String error = acc.validateWithdraw(amount);
-		if(error != null) {
-			System.out.println(error);
-		}
-		else {
+		try {
 			acc.withdraw(amount);
 			System.out.println("Novo saldo: "+ String.format("%.2f", acc.getBalance()));
+		}
+		catch(BusinessException e) {
+			System.out.println(e.getMessage());
 		}
 		sc.close();
 	}
